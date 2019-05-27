bfc3b6d36a77b2b6897755f2b7aa35792fbf5770
tomashauser <37981481+tomashauser@users.noreply.github.com>
2019-05-27 16:42:46 +0200

    Update Problem 1.c

--- Problem 1.c
+++ Problem 1.c
@@ -3,6 +3,6 @@
 int main() {
     int sum = 0;
     for (int i = 1; i < 1000; i++)
-        sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+        sum += (i % 3 == 0 || i % 5 == 0) ? i : 0; //just wanted to make it neat (adding 0 is slowing it down a bit)
     printf("%d\n", sum);
 }
