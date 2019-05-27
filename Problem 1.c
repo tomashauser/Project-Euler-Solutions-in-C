66fa4b24759e81653dd1863537a7fded22b0db93
tomashauser <37981481+tomashauser@users.noreply.github.com>
2019-05-27 16:50:50 +0200

    Delete Problem 1.c

--- Problem 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int sum = 0;
-    for (int i = 1; i < 1000; i++)
-        sum += (i % 3 == 0 || i % 5 == 0) ? i : 0; //just wanted to make it neat (adding 0 is slowing it down a bit)
-    printf("%d\n", sum);
-}
