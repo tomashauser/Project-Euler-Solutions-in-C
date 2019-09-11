80de3bce08f2f506bf8278d21a7dded2e42ceeb5
tomashauser <37981481+tomashauser@users.noreply.github.com>
2019-09-11 23:02:02 +0200

    Create 030.c

---
+++ 030.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int GetNumberLength(int number);
+int integerPow(int base, int exp);
+
+int main() {
+    int number = 1,  sumOfNumbers = 0, sumOfPowers;
+    while (number < 1000000) {
+        int numberLength = GetNumberLength(number), numberInArray[numberLength];
+        numberToArray(numberInArray, number);
+        for (int i = 0; i < numberLength; i++)
+         sumOfPowers += integerPow(numberInArray[i], 5);
+        if (sumOfPowers == number)
+            sumOfNumbers += number;
+        sumOfPowers = 0;
+        number++;
+    }
+    printf("%d\n", sumOfNumbers);
+    return 0;
+}
+
+void numberToArray(int *numberInArray, int number) {
+    int numberLength = GetNumberLength(number);;
+    int dividor = integerPow(10, numberLength - 1);
+    for (int i = 0; i < numberLength; i++) {
+        numberInArray[i] = number / dividor;
+        number -= numberInArray[i] * dividor;
+        dividor /= 10;
+    }
+}
+
+int GetNumberLength(int number) {
+    int numberLength = 0;
+    while (number != 0) {
+        number /= 10;
+        numberLength++;
+    }
+    return numberLength;
+}
+
+int integerPow(int base, int exp) {
+    int result = 1;
+    for (;;) {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}
